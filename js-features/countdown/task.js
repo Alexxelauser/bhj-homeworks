9051d156d367e2d768ba5317e38bef6f4a189797
Alex <kliver_sasha@mail.ru>
2023-11-22 14:16:05 +0300

    Доработка остановки таймера

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,11 +1,12 @@
 const startTimerMeaning  = function(){
-    const output = document.getElementById("timer");
-
+const output = document.getElementById("timer");
     if (output.textContent >= 1){
         output.textContent --;
-    }	else if(output.textContent = '0'){
+    }	else if (output.textContent = '0') {
+        setTimeout(() => { clearInterval(timerID); }, 0);
         alert("Вы победили в конкурсе");
     }
 }
+let timerID = setInterval(startTimerMeaning, 1000);
 
-setInterval(startTimerMeaning,1000);
+
